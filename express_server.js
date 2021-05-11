941346757f13816e58b97b49ab2f301a170ba6d2
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-05-11 21:42:22 +0000

    i am being a computer

--- express_server.js
+++ express_server.js
@@ -1,21 +1,22 @@
 const express = require("express");
-const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+const app = express();
+app.set("view engine", "ejs");
 
 // function generateRandomString() {
 
 // }
 
-
-app.set("view engine", "ejs");
+//middleware
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -59,5 +60,13 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  const deleteToggle = req.params.shortURL;
+  delete urlDatabase[deleteToggle];
+  res.redirect("/urls");
+});
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  res.redirect('urls');
+});
 
